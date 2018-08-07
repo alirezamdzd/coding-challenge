9f0afc85dd9489f7728e76bf59e366c05f523ee2
unknown <emailerdinesh@gmail.com>
2018-08-07 14:59:06 +0530

    task 1 solution-1

--- task#1/script.js
+++ task#1/script.js
@@ -1,18 +1,13 @@
 function myFunction(list) {
-  
-  for (var i = 0; i < list.length; i++) {
-
-    setTimeout(function () {
-      console.log(i);
-    }, 3000)
-
+  for (let i = 0; i < list.length; i++) {
+    setTimeout(function() {
+      console.log(list.indexOf(list[i]));
+    }, 3000);
   }
-  
 }
-
 
 var testArray = [10, 20, 30, 40, 50];
 myFunction(testArray);
 
-// To run the script, open your terminal window, navigate to current directory and enter command below: 
+// To run the script, open your terminal window, navigate to current directory and enter command below:
 // node script.js
